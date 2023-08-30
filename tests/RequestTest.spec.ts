81ffef1a944edfc093e9ad6f0e5631a67884330e
Marek Dev <marek@marekdev.me>
2023-08-30 12:22:16 +0100

    Add a simple request tests

--- tests/RequestTest.spec.ts
+++ tests/RequestTest.spec.ts
@@ -1,10 +1,63 @@
 import {describe} from "node:test";
 import TailorFetch, {TailorResponse} from "../src/index";
+import {assert, expect} from "chai";
 
 describe('make GET, POST, PUT, PATCH, DELETE requests', () => {
    it('should make a GET request and get data back', async () => {
 
-      const response: TailorResponse = await TailorFetch.GET('', { parseJSON: true });
+      const response: TailorResponse | undefined = await TailorFetch.GET('https://dummyjson.com/products/1', { parseJSON: true });
 
+      assert.equal(response?.successful(), true);
+      assert.instanceOf(response, TailorResponse);
+      expect(response?.data).not.be.undefined;
+   });
+
+   it('should make a POST request and get new data back', async () => {
+      const response = await TailorFetch.POST('https://dummyjson.com/products/add', { parseJSON: true, body: JSON.stringify({
+            title: 'example',
+            price: 200000
+         })});
+
+      assert.equal(response?.data.title, "example", "POST request failed to update data");
+
+      assert.equal(response?.successful(), true);
+      assert.instanceOf(response, TailorResponse);
+      expect(response?.data).not.be.undefined;
+   });
+
+   it('should make a PUT request and get new data back', async () => {
+      const response = await TailorFetch.POST('https://dummyjson.com/products/add', { parseJSON: true, body: JSON.stringify({
+            title: 'newItem',
+            price: 200000
+         })});
+
+      assert.equal(response?.data.title, "newItem", "PUT request failed to update data");
+
+      assert.equal(response?.successful(), true);
+      assert.instanceOf(response, TailorResponse);
+      expect(response?.data).not.be.undefined;
+   });
+
+   it('should make a PATCH request and get new data back', async () => {
+      const response = await TailorFetch.PATCH('https://dummyjson.com/products/1', { parseJSON: true, body: JSON.stringify({
+            title: 'newItem',
+            price: 200000
+         })});
+
+      assert.equal(response?.data.title, "newItem", "PUT request failed to update data");
+
+      assert.equal(response?.successful(), true);
+      assert.instanceOf(response, TailorResponse);
+      expect(response?.data).not.be.undefined;
+   });
+
+   it('should make a DELETE request and get deleted resource back', async () => {
+      const response = await TailorFetch.DELETE('https://dummyjson.com/products/1', { parseJSON: true });
+
+      assert.equal(response?.data.id, 1, "PUT request failed to update data");
+
+      assert.equal(response?.successful(), true);
+      assert.instanceOf(response, TailorResponse);
+      expect(response?.data).not.be.undefined;
    });
 });
