a497998726a99ffea68d0cde1bfcb45976e9d609
Marek Dev <marek@marekdev.me>
2023-09-08 09:35:55 +0000

    Add helper functions to TailorResponse

--- src/Response.ts
+++ src/Response.ts
@@ -95,4 +95,139 @@ export default class TailorResponse {
     serverError(): boolean {
         return this.status ? this.status >= 500 && this.status < 600 : false;
     }
+
+    /**
+     * 200 OK
+     * 
+     * @returns {boolean} Whether the request status was 200
+     */
+    ok(): boolean {
+        return this.status == 200;
+    }
+
+    /**
+     * 201 Created
+     * 
+     * @returns {boolean} Whether request status was 201
+     */
+    created(): boolean {
+        return this.status == 201;
+    }
+    
+    /**
+     * 202 Created
+     * 
+     * @returns {boolean} Whether request status was 202
+     */
+    accepted(): boolean {
+        return this.status == 202;
+    }
+
+    /**
+     * 204 No Content
+     * 
+     * @returns {boolean} Whether the request status was 204
+     */
+    noContent(): boolean {
+        return this.status == 204;
+    }
+
+    /**
+     * 301 Moved Permanently
+     * 
+     * @returns {boolean} Whether the request status was 301
+     */
+    movedPermanently(): boolean {
+        return this.status == 301;
+    }
+
+    /**
+     * 302 Found
+     * 
+     * @returns {boolean} Whether the request status was 302
+     */
+    found(): boolean {
+        return this.status == 302;
+    }
+
+    /**
+     * 400 Bad Request
+     * 
+     * @returns {boolean} Whether the request status was 400
+     */
+    badRequest(): boolean {
+        return this.status == 400;
+    }
+
+    /**
+     * 401 Unauthorized
+     * 
+     * @returns {boolean} Wether the request status was 401
+     */
+    unauthorized(): boolean {
+        return this.status == 401;
+    }
+
+    /**
+     * 402 Payment Required
+     * 
+     * @returns {boolean} Whether the request status was 402
+     */
+    paymentRequired(): boolean {
+        return this.status == 402;
+    }
+
+    /**
+     * 403 Forbidden
+     * 
+     * @returns {boolean} Whether the request status was 403
+     */
+    forbidden(): boolean {
+        return this.status == 403;
+    }
+
+    /**
+     * 404 Not Found
+     * 
+     * @returns {boolean} Whether the request status was 404
+     */
+    notFound(): boolean {
+        return this.status == 404;
+    }
+
+    /**
+     * 408 Request Timeout
+     * 
+     * @returns {boolean} Whether the request status was 408
+     */
+    requestTimeout(): boolean {
+        return this.status == 408;
+    }
+
+    /**
+     * 409 Conflict
+     * 
+     * @returns {boolean} Whether the request status was 409
+     */
+    conflict(): boolean {
+        return this.status == 409;
+    }
+
+    /**
+     * 422 Unprocessable Entity
+     * 
+     * @returns {boolean} Whether the request status was 422
+     */
+    unprocessableEntity(): boolean {
+        return this.status == 422;
+    }
+
+    /**
+     * 429 Too Many Requests
+     * 
+     * @returns {boolean} Whether the request status was 429
+     */
+    tooManyRequests(): boolean {
+        return this.status == 429;
+    }
 }
